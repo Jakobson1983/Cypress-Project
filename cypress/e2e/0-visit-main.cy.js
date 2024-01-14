b09f6ef1e4f3dd2ac77c51686c822eb7c95e8de7
Aszton <bartoszkuczera88@gmail.com>
2024-01-14 19:24:00 +0100

    pageObjects

--- cypress/e2e/0-visit-main.cy.js
+++ cypress/e2e/0-visit-main.cy.js
@@ -1,21 +1,23 @@
 describe("Visit & Login Page", () => {
-    beforeEach(() => {
-      cy.visit("/");
+  beforeEach(() => {
+    cy.visit("/");
+    // cy.xpath('//*[@id="customer_menu_top"]/li/a').click()
+  });
+  it("visit login url", () => {
+    cy.url().then((url) => {
+      cy.log(`The url is ${url}`);
+      expect(url).to.contain("/");
+      // cy.xpath("")
     });
-    it("visit login url", () => {
-      cy.url().then((url) => {
-        cy.log(`The url is ${url}`);
-        expect(url).to.contain("/");
-      });
-    });
-    it("Scroll to the bottom of the page", () => {
-      cy.scrollTo("bottom");
-    });
-    it("Scroll to the top of the page", () => {
-        cy.scrollTo("top");
-      });
-    it("Mouseover on the 'Login or register' menu", () => {
-        cy.get("span.menu_text").trigger("mouseover");
-        // cy.get("#customer_menu_top").should("be.visible");
-        });
+  });
+  it("Scroll to the bottom of the page", () => {
+    cy.scrollTo("bottom");
+  });
+  it("Scroll to the top of the page", () => {
+    cy.scrollTo("top");
+  });
+  it("Mouseover on the 'Login or register' menu", () => {
+    cy.get("span.menu_text").trigger("mouseover");
+    // cy.get("#customer_menu_top").should("be.visible");
+  });
 });
---
+++ cypress/support/selectors.js
@@ -0,0 +1,10 @@
+export const mainPageSelectors = {
+  buttonLoginOrRegister: "#customernav",
+};
+
+export const loginSelectors = {
+  inputLoginName: "#loginFrm_loginname",
+  inputPassword: "#loginFrm_password",
+  buttonLogin: '[title="Login"]',
+  alertDanger: ".alert-danger",
+};
